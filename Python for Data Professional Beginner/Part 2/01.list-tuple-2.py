56d5b1e017ad323ba5f8f857afa4e3a13cb42541
Ridwan Amshari <ridwanamshari0310@gmail.com>
2024-02-19 12:05:31 +0700

    feat: add List and Tuple - Part 2

---
+++ Python for Data Professional Beginner/Part 2/01.list-tuple-2.py
@@ -0,0 +1,8 @@
+bulan_pembelian = ('Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember')
+pertengahan_tahun = bulan_pembelian[4:8] # Mei, Juni, Juli, Agustus
+print(pertengahan_tahun)
+awal_tahun = bulan_pembelian[:5] # Januari, Februari, Maret, April, Mei
+print(awal_tahun)
+akhir_tahun = bulan_pembelian[8:] # September, Oktober, November, Desember
+print(akhir_tahun)
+print(bulan_pembelian[-4:-1]) # September, Oktober, November
