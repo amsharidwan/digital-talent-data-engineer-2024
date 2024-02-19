4ec9b45fe0dd057a52fe4b9557bc3d8b943f3eaf
Ridwan Amshari <ridwanamshari0310@gmail.com>
2024-02-19 13:48:47 +0700

    rename file

---
+++ Python for Data Professional Beginner/Part 2/00.list-and-tuple-1.py
@@ -0,0 +1,5 @@
+bulan_pembelian = ('Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember')
+print(bulan_pembelian[0]) # Januari
+print(bulan_pembelian[5]) # Juni
+print(bulan_pembelian[-1]) # Desember
+print(bulan_pembelian[-2]) # November
